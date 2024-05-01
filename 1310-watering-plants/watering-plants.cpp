d68ee9cbdff5191a56eda6bbe9516ae9dbf3e598
Salman Jan <jsalman862@gmail.com>
2024-05-01 16:40:52 +0500

    Time: 0 ms (100.00%) | Memory: 10.7 MB (44.17%) - LeetSync

--- 1310-watering-plants/watering-plants.cpp
+++ 1310-watering-plants/watering-plants.cpp
@@ -5,17 +5,11 @@ public:
         int water = capacity;
         for (int i = 0; i < plants.size(); i++) {
             steps++;
-            cout << "Steps " << steps << endl;
             water -= plants[i];
-            cout << "Water " << water << endl;
             if (i + 1 < plants.size() && water < plants[i + 1]) {
                 // refill the water
-                cout<<"Next Plant "<<plants[i+1]<<endl;
                 steps = steps + (2 * (i+1));
-                cout << "Steps " << steps << endl;
-
                 water = capacity;
-                cout << "Water " << water << endl;
             }
         }
         return steps;
